7f43802cf5a4ba885bf4773c8eed784c32759724
Marcel Mroz <mmroz.marcel@gmail.com>
2023-08-25 18:18:05 +0200

    minor changes

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+function Message() {
+    const name = 'Marcel';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+//https://www.youtube.com/watch?v=SqcY0GlETPk&t=375s
+
+export default Message;
